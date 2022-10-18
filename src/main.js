c1bed51a20545eecf8d17468a814e394f7184f1a
leohb07 <leohb1501@gmail.com>
2022-10-17 23:06:00 -0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+function setBgColorCC(props) {
+  const color = {
+    "visa": ["#099920", "#027702"],
+    "master": ["#088ff0", "#023d00"]
+  }
+
+ccBgColor01.setAttribute('fill', color[props][0])
+ccBgColor02.setAttribute('fill', color[props][1])
+}
+
+setBgColorCC("visa")
+globalThis.setBgColorCC = setBgColorCC
